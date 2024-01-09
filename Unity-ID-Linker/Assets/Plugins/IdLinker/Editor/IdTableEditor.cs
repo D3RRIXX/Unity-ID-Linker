4c3c3e1a55e8241147d442f1e14ac11e23e3ef09
D3RRIXX <jenyazagevskii@gmail.com>
2024-01-09 15:15:14 +0200

    Added logic for setting fields' values to "$MISSING" when an ID gets deleted

--- Unity-ID-Linker/Assets/Plugins/IdLinker/Editor/IdTableEditor.cs
+++ Unity-ID-Linker/Assets/Plugins/IdLinker/Editor/IdTableEditor.cs
@@ -8,30 +8,78 @@ namespace Derrixx.IdLinker.Editor
 	[CustomEditor(typeof(IdTable))]
 	public class IdTableEditor : UnityEditor.Editor
 	{
-		private ReorderableList _reorderableList;
+		private ReorderableList _list;
+		private SerializedProperty _definitionsProperty;
 
 		private void OnEnable()
 		{
-			_reorderableList = new ReorderableList(serializedObject, serializedObject.FindProperty("_idDefinitions"))
+			_definitionsProperty = serializedObject.FindProperty("_idDefinitions");
+			_list = new ReorderableList(serializedObject, _definitionsProperty)
 			{
 				displayAdd = true,
 				displayRemove = true,
-				onRemoveCallback = OnItemRemoved
+				onAddCallback = OnItemAdded,
+				onRemoveCallback = OnItemRemoved,
+				drawElementCallback = DrawElement,
+				elementHeightCallback = GetElementHeight,
+				drawHeaderCallback = DrawHeader,
 			};
+		}
+
+		private void OnItemAdded(ReorderableList list)
+		{
+			var arrayProperty = list.serializedProperty;
+			arrayProperty.InsertArrayElementAtIndex(arrayProperty.arraySize);
+			var elementProperty = arrayProperty.GetArrayElementAtIndex(arrayProperty.arraySize - 1);
+			elementProperty.FindPropertyRelative("Id").stringValue = string.Empty;
+			elementProperty.FindPropertyRelative("_assetRefs").ClearArray();
+
+			list.serializedProperty.serializedObject.ApplyModifiedProperties();
 		}
 
 		private void OnItemRemoved(ReorderableList list)
 		{
+			var arrayProperty = list.serializedProperty;
+
 			ReadOnlyCollection<int> indices = list.selectedIndices;
+
 			foreach (int index in indices)
 			{
-				Debug.Log(index);
+				RemoveIdFromReferencedAssets(arrayProperty.GetArrayElementAtIndex(index));
+				arrayProperty.DeleteArrayElementAtIndex(index);
 			}
 		}
 
+		private static void RemoveIdFromReferencedAssets(SerializedProperty idDefinitionProperty)
+		{
+			foreach (SerializedProperty refData in idDefinitionProperty.FindPropertyRelative("_assetRefs"))
+			{
+				SerializedProperty assetProperty = refData.FindPropertyRelative(nameof(RefData.Asset));
+				using var serObj = new SerializedObject(assetProperty.objectReferenceValue);
+				string propertyPath = refData.FindPropertyRelative(nameof(RefData.PropertyPath)).stringValue;
+
+				serObj.FindProperty(propertyPath).stringValue = "$MISSING";
+				serObj.ApplyModifiedProperties();
+			}
+		}
+
+		private void DrawHeader(Rect rect)
+		{
+			EditorGUI.LabelField(rect, "ID Table");
+		}
+
+		private void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
+		{
+			EditorGUI.PropertyField(rect, _definitionsProperty.GetArrayElementAtIndex(index), includeChildren: true);
+		}
+
+		private float GetElementHeight(int index) => EditorGUI.GetPropertyHeight(_definitionsProperty.GetArrayElementAtIndex(index), includeChildren: true);
+
 		public override void OnInspectorGUI()
 		{
-			_reorderableList.DoLayoutList();
+			serializedObject.Update();
+			_list.DoLayoutList();
+			serializedObject.ApplyModifiedProperties();
 		}
 	}
 }
