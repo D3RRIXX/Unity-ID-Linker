ac15646cbdc65b0474453c7afd0f47d24f4f5e1f
D3RRIXX <jenyazagevskii@gmail.com>
2024-01-09 13:50:21 +0200

    Added sample ID user Scriptable Object script

---
+++ Unity-ID-Linker/Assets/Scripts/ItemIdUser.cs
@@ -0,0 +1,9 @@
+﻿using Derrixx.IdLinker;
+using UnityEngine;
+
+[CreateAssetMenu(order = -90, fileName = "New Item")]
+public class ItemIdUser : ScriptableObject
+{
+	[SerializeField, ItemId] private string _id;
+	[SerializeField, ItemId] private int _test;
+}
