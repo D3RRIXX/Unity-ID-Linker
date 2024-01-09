2d19b894de53c0f715a4efe6e72bd3141b503538
D3RRIXX <jenyazagevskii@gmail.com>
2024-01-09 14:42:03 +0200

    Made custom drawer for IdDefinition. Currently updates existing refs

---
+++ Unity-ID-Linker/Assets/Plugins/IdLinker/Editor/IdDefinitionDrawer.cs
@@ -0,0 +1,58 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Derrixx.IdLinker.Editor
+{
+	[CustomPropertyDrawer(typeof(IdDefinition))]
+	public class IdDefinitionDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			var idProperty = property.FindPropertyRelative(nameof(IdDefinition.Id));
+			string oldId = idProperty.stringValue;
+
+			var rect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+
+			EditorGUI.BeginChangeCheck();
+			EditorGUI.DelayedTextField(rect, idProperty, GUIContent.none);
+
+			position.y += EditorGUIUtility.singleLineHeight;
+			position.height -= EditorGUIUtility.singleLineHeight;
+
+			var assetRefsProperty = property.FindPropertyRelative("_assetRefs");
+			EditorGUI.PropertyField(position, assetRefsProperty);
+
+			if (EditorGUI.EndChangeCheck() && !string.Equals(idProperty.stringValue, oldId))
+			{
+				// Update fields of all asset refs
+				foreach (SerializedProperty refData in assetRefsProperty)
+				{
+					UpdatedFieldValue(refData, idProperty.stringValue);
+				}
+
+				Debug.Log("Made changes");
+			}
+		}
+
+		private static void UpdatedFieldValue(SerializedProperty refData, string newId)
+		{
+			using var serObj = new SerializedObject(refData.FindPropertyRelative(nameof(RefData.Asset)).objectReferenceValue);
+			string propertyPath = refData.FindPropertyRelative(nameof(RefData.PropertyPath)).stringValue;
+			serObj.FindProperty(propertyPath).stringValue = newId;
+			serObj.ApplyModifiedProperties();
+		}
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			float lineHeight = EditorGUIUtility.singleLineHeight;
+			
+			float height = lineHeight;
+			while (property.NextVisible(true))
+			{
+				height += lineHeight;
+			}
+
+			return height;
+		}
+	}
+}
