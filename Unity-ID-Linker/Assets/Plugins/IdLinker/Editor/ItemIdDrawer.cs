7e2b5af6292c79aad465310c4f39674297acaade
D3RRIXX <jenyazagevskii@gmail.com>
2024-01-09 15:15:30 +0200

    Now processing special ID values in the drawer

--- Unity-ID-Linker/Assets/Plugins/IdLinker/Editor/ItemIdDrawer.cs
+++ Unity-ID-Linker/Assets/Plugins/IdLinker/Editor/ItemIdDrawer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -8,6 +9,8 @@ namespace Derrixx.IdLinker.Editor
 	[CustomPropertyDrawer(typeof(ItemIdAttribute))]
 	public class ItemIdDrawer : PropertyDrawer
 	{
+		private static readonly Regex SPECIAL_VALUE_REGEX = new("([A-Z])\\w+");
+
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
 			if (property.propertyType is not SerializedPropertyType.String)
@@ -15,11 +18,18 @@ namespace Derrixx.IdLinker.Editor
 				EditorGUI.LabelField(position, label.text, "Item ID must be a string.");
 				return;
 			}
-
+			
 			EditorGUI.BeginChangeCheck();
+			
+			string currentValue = property.stringValue;
+			bool hasSpecialValue = currentValue.StartsWith('$');
 			string[] options = GetDisplayedOptions();
-			int initialIndex = Array.IndexOf(options, property.stringValue);
+			
+			int initialIndex = Array.IndexOf(options, currentValue);
 			int index = EditorGUI.Popup(position, label.text, initialIndex, options);
+			if (hasSpecialValue)
+				DrawSpecialValue(position, currentValue);
+			
 			if (EditorGUI.EndChangeCheck())
 			{
 				property.stringValue = options[index];
@@ -37,6 +47,31 @@ namespace Derrixx.IdLinker.Editor
 			}
 		}
 
-		private static string[] GetDisplayedOptions() => IdTable.Instance.Ids;
+		private static void DrawSpecialValue(Rect position, string currentValue)
+		{
+			position = new Rect(position);
+			position.x += EditorGUIUtility.labelWidth;
+			position.width -= EditorGUIUtility.labelWidth;
+
+			currentValue = SPECIAL_VALUE_REGEX.Match(currentValue).Value;
+			
+			var style = new GUIStyle(EditorStyles.popup)
+			{
+				normal =
+				{
+					textColor = currentValue switch
+					{
+						"MISSING" => Color.red,
+					}
+				}
+			};
+
+			EditorGUI.LabelField(position, string.Empty, currentValue, style);
+		}
+
+		private static string[] GetDisplayedOptions()
+		{
+			return IdTable.Instance.Ids;
+		}
 	}
 }
